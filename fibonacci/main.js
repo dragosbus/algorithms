76b98f3adea5cd872adb13a8552eb64de59b6cd1
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-04-06 17:37:05 +0200

    refactor fibonacci
    added all code in a function using fat arrow from es6

--- fibonacci/main.js
+++ fibonacci/main.js
@@ -1,16 +1,19 @@
-var fib = [0, 1];
-var sum = 0;
-
-for (var i = 2; i < 4000000; i++) {
+const fib = n => {
+    var fib = [0, 1];
+    var sum = 0;
+    
+    for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
     //Check if value exceed 4million
-    if (fib[i] > 4000000) {
+    if (fib[i] > n) {
         break;
-    }
-    //Make sum of all even numbers from serie
+    } 
+     //Make sum of all even numbers from serie
     if (fib[i] % 2 === 0) {
         sum += fib[i];
     }
 }
+    return fib;
+}
 
-console.log(sum);
+console.log(fib(4));
